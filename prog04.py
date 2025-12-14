fb4fad5bbf57cc40551ac529a2dfca6e6d739163
B Mikhail <335097@mail.ru>
2025-12-14 11:45:57 +0700

    1214

---
+++ prog04.py
@@ -0,0 +1,7 @@
+num = 33
+last_digit = num % 10
+first_digit = num // 10
+if last_digit == first_digit:
+    print ('11')
+else:
+    print ('12')
---
+++ prog05.py
@@ -0,0 +1,9 @@
+num1, num2, num3 = 2, 8, 3 # множественное присваивание num1 = -8, num2 = 2, num3 = 6
+count = 0
+if num1 % 2 == 0:
+    count = count + 1
+if num2 % 2 == 0:
+    count = count + 1
+if num3 % 2 == 0:
+    count = count + 1
+print(count)
