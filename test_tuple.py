ac72007857cc60d4022c577ba9f75a1c07570d11
B Mikhail <335097@mail.ru>
2025-12-14 20:00:35 +0700

    123

--- test_tuple.py
+++ test_tuple.py
@@ -1,17 +1,5 @@
-my_set = {1, 2, 2, 3}
-print(my_set)
-print(1 in my_set)
-print(4 in my_set)
-my_set.add(4)
-my_set.discard(2)
-my_set.remove(3)
-my_set.pop()
-print(my_set)
-
-my_dict = {
-    "name": "dfgdfg",
-    "age": "35",
-    "city": "dfgdghjgfg"
-}
-print(my_dict["name"])
-
+s ={"Python"}
+s.add("fghfgfghfghhfgh")
+s.add("njcvr")
+s.remove("fghfgfghfghhfgh")
+print(s)
